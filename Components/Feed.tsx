c9cb50df4471a1cc874edc0fc49e0ed5e7e94f39
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-17 16:52:13 +0100

    Feed formatting commit

--- Components/Feed.tsx
+++ Components/Feed.tsx
@@ -6,9 +6,18 @@ import React from 'react'
 function Feed() {
   return (
     <div>
-        <div>
-            <h1>Home</h1>
-            <RefreshIcon/>
+           {/* Top of Feed  */}
+           {/* give flex property so that are made into a row 
+           and centered in the axis and be pushed apart by justify-between*/}
+        <div className='flex items-center justify-between'>
+
+            {/* Add style 
+            Home= padding extra large and bold*/}
+            <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
+
+            {/* Style refresh icon to animate and ease out of animation */}
+            <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter
+            transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
         </div>
 
     </div>
