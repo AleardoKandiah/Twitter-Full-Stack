fb472a789fce5462926305c5b6af84dd5d8ff5ce
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-19 12:22:07 +0100

    Feed form and tweet commit

--- Components/TweetBox.tsx
+++ Components/TweetBox.tsx
@@ -4,7 +4,27 @@ function TweetBox() {
   return (
     <div>
       {/* object cover to take avai;able space */}
-        <img className='h-14 w-14 object-cover rounded-full' src="https://links.papareact.com/gll" alt="" />
+        <img className='h-14 w-14 object-cover rounded-full mt-4' 
+        src="https://links.papareact.com/gll" 
+        alt="" 
+        />
+
+      {/* Build submission form which allows to tweet     */}
+    <div>
+      
+      <form>
+        <input type='text' placeholder="What's Happening?"/>
+
+        {/* Build a div for tweet icons and submission */}
+        <div>
+          <div> Icons </div>
+
+          <form>Tweet</form>
+
+        </div>
+      </form>
+    </div>
+
     </div>
   )
 }
