b83a2e1ae0e38a5a00ac74bd9af3ca331b765f2f
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-19 11:48:25 +0100

    Tweetbox commit

--- Components/TweetBox.tsx
+++ Components/TweetBox.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 function TweetBox() {
   return (
     <div>
-        <img className='h-14 w-14 object-cover' src="https://links.papareact.com/gll" alt="" />
+      {/* object cover to take avai;able space */}
+        <img className='h-14 w-14 object-cover rounded-full' src="https://links.papareact.com/gll" alt="" />
     </div>
   )
 }
