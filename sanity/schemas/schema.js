ab0e11c22b9c6b7ee4bf0674fa38b903576f8a3f
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-21 16:10:04 +0100

    sanitiy package configuration

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,3 +1,4 @@
+// This is where everything gets tied together
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 
@@ -5,25 +6,15 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import tweet from './tweet'
+import comment from './comment'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
+  // Name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    post,
-    author,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-  ]),
+  // Then we concatenate our document type
+  // to ones provided from plugins that are installed
+  type: schemaTypes.concat([ tweet,comment ]),
 })
