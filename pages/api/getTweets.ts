323539d088af983ba668e202ec572038e248637a
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-22 20:03:39 +0100

    sanity CMS back-end commit

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+// import sanity client
+import {sanityClient} from '../../sanity'
+// import typyings d
+import {Tweet} from '../../typings'
 type Data = {
-  name: string
+    // define what the tweet response we want looks like
+  tweets: Tweet[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    // use grock to query sanity CMS and grab backend API
+    
+    res.status(200).json({ name: 'John Doe' })
 }
