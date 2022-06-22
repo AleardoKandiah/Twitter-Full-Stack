82f00a402867979033ce7404c0b49fe8692553f4
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-22 19:49:47 +0100

    Client accessible tweet body commit

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,13 @@
 // .d.td for type definition files in order to define what a tweet is
+// the tweet APi comes with a TON of info
+// this need to be broken up into tweet and body
+
+// this const wil be accessible to the client
+export const TweetBody ={
+    text: string,
+    username: string,
+    prolfileImg: string,
+    // the image will be optional and not 100% required and so ?
+    Image?: string 
+
+}
