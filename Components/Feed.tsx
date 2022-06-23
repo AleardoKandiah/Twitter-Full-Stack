8af5db748a61b14ffd5dda2e416ec05512768b98
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-23 21:05:52 +0100

    comment commit

--- Components/Feed.tsx
+++ Components/Feed.tsx
@@ -9,7 +9,7 @@ function Feed() {
   return (
 
     // Main Feed given a column size of 5 ot of 9
-    // At smaller screen serialize, the feed will take up the additional 2 coplumns from the widget
+    // At smaller screen serialize, the feed will take up the additional 2 columns from the widget
     // At default the feed wil take up 7 columns, whichis then increased to 7 at larger screen size
     <div className='col-span-7 lg:col-span-5 border-x'>
            {/* Top of Feed  */}
