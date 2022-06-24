407959cbd23b44f5b4e1d7064c98e110bebb0b3c
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-24 11:46:25 +0100

    tweet data retrieval

--- utils/fetchTweet.ts
+++ utils/fetchTweet.ts
@@ -1,8 +1,18 @@
 // utility folder for functions 
 
+import { Tweet } from '../typings';
+
 // create a async function for fetchingtweets
 export const fetchTweets = async () => {
     // connect to backend endpoint
-
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`)
+
+    // next we need to retreive the data
+    const data = await res.json();
+    // cast tweets inside
+    // we can also use destructuring here
+    const tweets: Tweet[] = data.tweets;
+
+    // by returning we can now use it on the front end
+    return tweets
 }
